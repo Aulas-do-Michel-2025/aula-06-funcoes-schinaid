243f07770019cf711a0ad108be9b3267f13ac345
Schinaid <andersonschinaid@hotmail.com>
2025-06-13 10:10:57 -0300

    Update exercicio_2.py

--- exercicio_2.py
+++ exercicio_2.py
@@ -8,3 +8,20 @@ Exemplo de uso:
 >>> print(criar_organismo(10, 'HIV', 1000))
 >>> {"id": 10, "nome": "HIV", 1000}
 """
+def criar_organismo(id, nome, tamanho_do_genoma):
+    """
+    Cria e retorna um dicionário representando um organismo.
+
+    Parâmetros:
+    - id: identificador do organismo
+    - nome: nome do organismo (string)
+    - tamanho_do_genoma: tamanho do genoma (número)
+
+    Retorna:
+    Um dicionário com as chaves "id", "nome" e "tamanho_do_genoma".
+    """
+    return {
+        "id": id,
+        "nome": nome,
+        "tamanho_do_genoma": tamanho_do_genoma
+    }
