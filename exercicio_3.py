e0c037b18dedf382781b4bac2f4f0f5cf335937c
Schinaid <andersonschinaid@hotmail.com>
2025-06-13 10:13:56 -0300

    Update exercicio_3.py

--- exercicio_3.py
+++ exercicio_3.py
@@ -8,4 +8,9 @@ Exemplo de uso:
 >>> print(calcular_media([0, 100, 200]))
 >>> 100
 """
+def calcular_media(numeros):
+    """
+    Recebe uma lista de números e retorna a média dos valores.
+    """
+    return sum(numeros) / len(numeros)
 
